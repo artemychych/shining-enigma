cacd63de0aa81099d5bee499cb7c418442f28054
Vladimir Leveryev <volodya.leveryev@gmail.com>
2024-07-27 14:19:47 +0900

    Добавить главную страницу

--- paperwork/apps.py
+++ paperwork/apps.py
@@ -4,4 +4,4 @@ from django.apps import AppConfig
 class PaperWorkConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'paperwork'
-    verbose_name = 'Paper work'
+    verbose_name = 'Бумажная работа'
