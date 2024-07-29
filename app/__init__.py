d2adacbe2d53bd87ddeed53295ad830a0accf45a
Vladimir Leveryev <volodya.leveryev@gmail.com>
2024-07-30 08:31:39 +0900

    Google Auth

--- app/__init__.py
+++ app/__init__.py
@@ -1,3 +1,5 @@
+import json
+
 from flask import Flask
 
 from app import views
@@ -5,5 +7,11 @@ from app import views
 
 def create_app():
     app = Flask(__name__)
-    app.add_url_rule('/', endpoint='index_page', view_func=views.index_page)
+
+    app.config.from_file('config.json', load=json.load)
+
+    app.add_url_rule('/', view_func=views.index_page)
+    app.add_url_rule('/login/', view_func=views.login, methods=['POST'])
+    app.add_url_rule('/logout/', view_func=views.logout)
+
     return app
--- app/views.py
+++ app/views.py
@@ -1,6 +1,63 @@
-from flask import render_template
+from functools import wraps
+
+from flask import (abort, current_app, render_template, redirect, request,
+                   session, url_for)
+from google.oauth2 import id_token
+from google.auth.exceptions import GoogleAuthError
+from google.auth.transport import requests
+
+
+def login_required(f):
+    @wraps(f)
+    def decorated_function(*args, **kwargs):
+        if 'email' not in session:
+            return redirect(url_for('login', next=request.url))
+        return f(*args, **kwargs)
+    return decorated_function
 
 
 def index_page():
-    context = {}
-    return render_template('index.html', **context)
+    client_id = current_app.config['CLIENT_ID']
+    return render_template('index.html', client_id=client_id)
+
+
+def login():
+    # Защита от CSRF
+    cookie_token = request.cookies.get('g_csrf_token')
+    if not cookie_token:
+        return abort(400, 'No CSRF token in Cookie.')
+    post_token = request.form.get('g_csrf_token')
+    if not post_token:
+        return abort(400, 'No CSRF token in post body.')
+    if cookie_token != post_token:
+        return abort(400, 'Failed to verify double submit cookie.')
+
+    # Проверка токена
+    credential = request.form.get('credential')
+    if not credential:
+        return abort(400, 'No ID token.')
+    try:
+        req = requests.Request()
+        client_id = current_app.config['CLIENT_ID']
+        id_info = id_token.verify_oauth2_token(credential, req, client_id)
+    except GoogleAuthError:
+        return abort(401, 'ID token verification error.')
+
+    # Проверим что пользователь зарегистрирован
+    email = id_info['email']
+    if email not in current_app.config['USERS']:
+        return abort(403, 'User is not authorized.')
+
+    session['email'] = email
+    session['username'] = id_info['name']
+    session['userid'] = id_info['sub']
+    next_url = request.args.get('next') or url_for('index_page')
+    return redirect(next_url)
+
+
+def logout():
+    session.pop('email')
+    session.pop('username')
+    session.pop('userid')
+    next_url = request.args.get('next') or url_for('index_page')
+    return redirect(next_url)
